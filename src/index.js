f1eb6cf58ccc1a46bd4ca8175cc9da813b96ea9d
Maxim Maltsev <mmi3220702@gmail.com>
2021-02-23 21:05:51 +0300

    task completion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let arr = [];
+
+  bracketsConfig.forEach(element => {
+    arr.push(element.join(''));
+  });
+
+  for (let i = 0; i < arr.length; i++) {
+    if (str.includes(arr[i])) {
+      str = str.replace(arr[i], '');
+      i = -1;
+    }
+  }
+  if (str.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
